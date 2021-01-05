b511d347c6fbeca070a6bea3aa78466d8b5a3023
Your Name <you@example.com>
2021-01-04 22:00:02 -0300

    Subida actividad primer progarma java

---
+++ TuPropioProgramaJava/Presentacion.java
@@ -0,0 +1,10 @@
+public class Presentacion {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Mi nombre es Cristian Vásquez");
+        System.out.println("Tengo 31 años de edad");
+        System.out.println("Mi ciudad es Temuco");
+	}
+
+}
